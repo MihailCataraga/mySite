526898d4da9220d762103455d6e568b8a64e2939
Mihai <cataraga.95@gmail.com>
2023-02-08 16:23:11 +0200

    Let's start with Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div className='navbar'>
+        <div className='logo'>
+            <h1><span>MC</span>website</h1>
+        </div>
+        <div className='pages'>
+            
+        </div>
+    </div>
+  )
+}
