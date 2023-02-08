fcc459763bb9201743c6412e9c30e0369fb7a57c
Mihai <cataraga.95@gmail.com>
2023-02-08 18:16:25 +0200

    test

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-        <div className='logo'>
+    <div className='navbar d-flex a-center'>
+        <Link to={"/"} className='logo'>
             <h1><span>MC</span>website</h1>
-        </div>
+        </Link>
         <div className='pages'>
-            
+            <Link className='link' to={"/"}>Home</Link>
+            <Link className='link' to={"/"}>Projects</Link>
+            <Link className='link' to={"/"}>About Me</Link>
+            <Link className='link' to={"/"}>Contacts</Link>
         </div>
     </div>
   )
